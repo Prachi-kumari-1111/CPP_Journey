fcc6ffd2eb6067c0e2198293b05d9dfb2818efd2
Prachi-kumari-1111 <prachikri8840@gmail.com>
2026-02-22 21:26:00 +0530

    e:\CPP_Learning_Path\CPP_Journey\Basics\hello.cpp

---
+++ Basics/hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  cout << "Prachi" "\n";
+  cout << "Kumari" "\n";
+  return 0;
+}
+
+// or bhi tarike se likh skte hai jaise  "<< endl" laga ke
+
+// Target ye tha ki name alg alg line me print ho
+// -prachi
+// -kumari

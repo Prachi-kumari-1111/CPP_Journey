84a235cd96be837104e79039c7782dcfc48bc97e
Prachi-kumari-1111 <prachikri8840@gmail.com>
2026-02-22 21:26:58 +0530

    e:\CPP_Learning_Path\CPP_Journey\Basics\Input_in.cpp

---
+++ Basics/Input_in.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    double price;
+  cout << "Enter Your price: ";
+  cin >> price;
+
+  cout << "You entered a price: " << price << endl;
+  return 0;
+}

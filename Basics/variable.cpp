dfd21b223bdb4de4a64ae30f5737e841c640ec1c
Prachi-kumari-1111 <prachikri8840@gmail.com>
2026-02-23 17:16:14 +0530

    e:\CPP_Learning_Path\CPP_Journey\Basics\variable.cpp

---
+++ Basics/variable.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int age = 25;
+  char Grade = 'A';
+  float PI = 3.14;
+  bool isSafe = true;
+  double price = 100.05;
+  cout << price << endl;
+}

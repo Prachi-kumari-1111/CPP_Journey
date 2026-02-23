8b3f8359676deddcff988cd50e6020c1f2b97ade
Prachi-kumari-1111 <prachikri8840@gmail.com>
2026-02-23 17:14:55 +0530

    e:\CPP_Learning_Path\CPP_Journey\Basics\typecasting_explicit.cpp

---
+++ Basics/typecasting_explicit.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main (){
+    
+  double price = 100.99;  //Decimal se pahle tak ka digit hi print hota hai hamesa..
+  int newPrice = (int)price;
+
+ cout << newPrice << endl;
+ return 0;
+}

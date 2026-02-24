3a60870f1ceab3736ba89d47d4b76e4ef73bc1b8
Prachi-kumari-1111 <prachikri8840@gmail.com>
2026-02-24 20:25:47 +0530

    e:\CPP_Learning_Path\CPP_Journey\Basics\arithemetic.cpp

---
+++ Basics/arithemetic.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+ int a , b;
+ cout<<"Enter a :" <<endl;
+ cin >>a;
+ 
+ cout<<"Enter b :" <<endl;
+ cin >> b;
+ 
+    cout << "sum =" << a + b << endl;
+    cout <<"difference =" << a - b << endl;
+    cout <<"product=" << a*b << endl;
+    cout <<"division =" << a/b<< endl;
+    cout << "modulo =" << a%b << endl;
+    return 0;
+}

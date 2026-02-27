fbf3fb317190be9f8f6464a84ec91e4500ab21e4
Prachi-kumari-1111 <prachikri8840@gmail.com>
2026-02-27 20:29:30 +0530

    e:\CPP_Learning_Path\CPP_Journey\Basics\conditional.cpp

---
+++ Basics/conditional.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main () {
+  int n = 17;
+    if (n>=0){
+      cout << "n is positive" << endl;
+    } else {
+        cout << "n is negative" << endl;
+    }
+    return 0;
+}

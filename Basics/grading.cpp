07fa07933527909a83cda71353a3fb19ddf9acd7
Prachi-kumari-1111 <prachikri8840@gmail.com>
2026-03-01 20:18:48 +0530

    e:\CPP_Learning_Path\CPP_Journey\Basics\grading.cpp

---
+++ Basics/grading.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+  int marks;
+  cout << "Enter marks: ";
+  cin >> marks;
+  
+  if (marks >= 90){
+    cout << "Grade A \n";
+  }else if (marks>=80 && marks<90){
+    cout << "Grade B \n";
+  }else{
+    cout << "Grade c \n";
+  }
+  return 0;
+}
